fd2bc552d96487cea4220811ec1f64f779a949f6
Pikatsuto <pikatsuto@gmail.com>
2024-08-14 23:44:37 +0200

    feat: add fprint support

--- system/default/services.nix
+++ system/default/services.nix
@@ -15,6 +15,15 @@
       autoLogin.enable = true;
       autoLogin.user = primaryUser;
     };
+    fprintd = {
+      enable = true;
+      package = pkgs.fprintd-ft9201;
+    };
+    udev.extraRules = ''
+      # FocalTech Systems Co., Ltd Fingerprint
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="2808", ATTRS{idProduct}=="93a9", ATTRS{dev}=="*", TEST=="power/control", ATTR{power/control}="auto", MODE="0660", GROUP="plugdev"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="2808", ATTRS{idProduct}=="93a9", ENV{LIBFPRINT_DRIVER}="FocalTech Systems Co., Ltd Fingerprint"
+    '';
   };
 ########################################################################
 }
