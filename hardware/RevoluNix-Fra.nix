61853b65d4b435e43ce5694e0baefa25e6b53a6d
Pikatsuto <pikatsuto@gmail.com>
2024-11-17 22:30:35 +0100

    fix: update hardware

--- hardware/RevoluNix-Fra.nix
+++ hardware/RevoluNix-Fra.nix
@@ -8,34 +8,41 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" "uas" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "uas" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.supportedFilesystems = [ "ntfs" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/e27cdd31-5f7d-4aa9-ad10-5b1e0fd4becf";
-      fsType = "ext4";
-    };
-
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/7dc26437-5ec9-46d1-9b26-b0500a68553f";
-      fsType = "ext4";
-    };
-
-  fileSystems."/var/log" =
-    { device = "/dev/disk/by-uuid/191e3594-a7fe-4227-bad5-c90a19d33e5e";
+    { device = "/dev/disk/by-uuid/cdcfac99-2cb7-4a7e-adea-b6988fd319f8";
       fsType = "ext4";
     };
 
   fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/AF1D-2CCF";
+    { device = "/dev/disk/by-uuid/4CAC-BFE5";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
+  fileSystems."/var" =
+    { device = "/dev/disk/by-uuid/a75cb4d5-65fd-46d5-badd-8ee02cf89ed7";
+      fsType = "ext4";
+    };
+
+  fileSystems."/home/gabriel" =
+    { device = "/dev/disk/by-uuid/2D02510A069478E8";
+      fsType = "ntfs-3g";
+      options = [ "rw" "uid=1000" "gid=100" "umask=077" ];
+    };
+
+  fileSystems."/var/log" =
+    { device = "/dev/disk/by-uuid/ad7a1fae-d00e-43b5-bc80-82e60a61ced6";
+      fsType = "ext4";
+    };
+
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/94138753-4d9c-444e-9aa6-25e60bb906b6"; }
+    [ { device = "/dev/disk/by-uuid/55bc475c-495a-4907-94e3-b9beb75370ff"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -43,7 +50,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp197s0f3u2u3.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp199s0f3u1.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
