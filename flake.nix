9cdcfc13cf82660e89f4ab84fbf99e578981d8a2
Pikatsuto <pikatsuto@gmail.com>
2024-11-17 22:32:40 +0100

    test: update hyprland to unstable

--- flake.nix
+++ flake.nix
@@ -8,9 +8,11 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://cache.saumon.network/proxmox-nixos"
+      "https://hyprland.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "proxmox-nixos:nveXDuVVhFDRFx8Dn19f1WDEaNRJjPrF2CPD2D+m1ys="
     ];
   };
